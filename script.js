d81a4880fc43167afffdda34c88e3731503eb18c
Aditi <aditi17sawant@gmail.com>
2023-10-27 23:14:52 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-gsap.from("#page1 h1"),{
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+gsap.from("#page1 h1",{
 	y:100,
 	opacity:0,
 	delay:0.5,
 	duration:0.9,
 	stagger:0.2
-}
+});
